05a7ef5a96b5841b1a334416571e5b02ff645a0d
Gustavo Pires <gustavomp.ti@gmail.com>
2019-04-12 20:49:08 -0300

    atividade2

---
+++ Atividade2/Agenda/Migrations/20190412233320_initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Agenda.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    ContactId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 30, nullable: false),
+                    Phone = table.Column<string>(maxLength: 14, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.ContactId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
---
+++ Atividade2/Agenda/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Agenda.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite seu nome"), MaxLength(30)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Digite o telefone"), MaxLength(14)]
+        public string Phone { get; set; }
+    }
+}
